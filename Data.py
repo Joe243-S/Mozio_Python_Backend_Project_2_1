8fe00bbb1306c367dbc9bfab60d217d81de3d21a
Joe243-S <99380232+Joe243-S@users.noreply.github.com>
2022-06-05 20:48:14 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -2,6 +2,6 @@ import pymssql
 
 
 def get_connection():
-    data = pymssql.connect(server='mysqlserve243.database.windows.net', user='lutende',
+    data = pymssql.connect(server='mysqlserve243.database.windows.net', port=1433, user='lutende',
                            password='Joem243@', database='myDataba')
     return data
